a0c74c103644f6cf2fa0346fc977fa3681dbf407
Ema Čampa <emacampa3@gmail.com>
2022-11-04 23:13:40 +0100

    added functions

---
+++ DN/računanje-optimalno.py
@@ -0,0 +1,64 @@
+import random
+
+op = input("Katero operacijo želiš trenirati? 1 - seštevanje; 2 - odštevanje; 3- množenje; 4- deljenje; 5 - naključno: ")
+zahtevnost = input ("kako težko želiš? 1-4: ")
+
+if zahtevnost == "1":
+    maks = 10
+elif zahtevnost == "2":
+    maks = 20
+elif zahtevnost == "3":
+    maks = 50
+else:
+    maks = 100
+
+vseh = 0
+pravilnih = 0
+
+while True:
+    # tukaj bo spraševal po številih itd
+    
+    prvo = random.randint(1, maks)
+    drugo = random.randint(1, maks)
+
+    if op == '5':
+        operacija = random.randint(1,4)
+    else:
+        operacija = int(op)
+
+    if operacija == '4' and prvo%drugo!=0:
+        continue
+
+
+    if operacija == 1:
+        rezultat = prvo + drugo
+        poziv = '+'
+    elif operacija == 2:
+        rezultat = prvo - drugo
+        poziv = '-'
+    elif operacija == 3:
+        rezultat = prvo * drugo
+        poziv = '*'
+    else:
+        rezultat = prvo / drugo
+        poziv = '/'
+        
+
+    mnenje = input(f"Izračunaj: {prvo} {poziv} {drugo}: ")
+
+    if mnenje == "x":
+        if vseh > 10:
+            break
+        else:
+            print ("Ne moreš še ven!")
+            continue
+    elif int(mnenje) == rezultat:
+        print ("Pravilno!")
+        pravilnih+=1
+    else:
+        print ("Narobe, pravilen rezultat je", rezultat)
+            
+    vseh+=1
+
+print ("Uspešnost: ", (pravilnih/vseh)*100, '%')
+
--- funkcije.py
+++ funkcije.py
@@ -1,3 +1,4 @@
+import random
 
 # pobriše terminal
 def pobrisi():
@@ -34,3 +35,116 @@ rezultat2 = koren(64, 3) # vrednost k lahko prepišemo
 print(f"kubicni koren števil 64 je {rezultat2}")
     
 
+# glede na vnešene katete s funkcijo izračunamo hipotenuzo
+def hipotenuza(kateta1, kateta2):
+  return ((kateta1**2 + kateta2**2) ** 0.5)
+
+kateta1 = int(input("prva kateta: "))
+kateta2 = int(input("druga kateta: "))
+print(f"dolžina hipotenuze {hipotenuza(kateta1, kateta2)}") # calling the function znotraj print
+
+# napredna verzija
+def pravilni_tiroktnik(stranica1, stranica2, operacija):
+  if operacija == "k":
+    return ((stranica1**2 - stranica2**2) ** 0.5)
+  elif operacija == "h":
+    return ((stranica1**2 + stranica2**2) ** 0.5)
+
+operacija = input("izračun (k)atete ali (h)ipotenuze: ")
+if operacija == "h":
+  stranica1 = int(input("dolžina katete: "))
+else:
+  stranica1 = int(input("dolžina hipotenuze: "))
+
+stranica2 = int(input("druga stranica: "))
+print(f"rešitev: {pravilni_tiroktnik(stranica1, stranica2, operacija)}")
+
+# še naprednejša verzija
+# vedno posredujemo dve števili: k1 in k2 oz. k1/k2 in h
+def p_trikotnik(kateta1=0, kateta2=0, hipotenuza=0):
+  if kateta2 != 0 and kateta1 == 0 and hipotenuza != 0:
+    kateta1 = kateta2 # če posredujemo kateto2 in hipotenuzo, kateto2 preslikamo v kateto1
+    kateta2 = 0
+
+  if kateta2 == 0 and kateta2 != 0 and hipotenuza != 0:
+    return ((hipotenuza**2)-(kateta1**2))**0.5
+  elif kateta1 != 0 and kateta2 != 0 and hipotenuza == 0:
+    return ((kateta1**2) + (kateta2**2)) ** 0.5
+
+  if (kateta1 == 0 and kateta2 == 0) or (kateta1 == 0 and hipotenuza == 0) or (kateta2 == 0 and hipotenuza == 0):
+    return "izračun ni možen le z 1 parametrom"
+
+k1 = int(input("dolžina katete 1: "))
+k2 = int(input("dolžina katete 2: "))
+print(f"hipotenuza je: {p_trikotnik(kateta1=k1, kateta2=k2)}")
+
+k = int(input("dolžina katete: "))
+h = int(input("dolžina hipotenuze: "))
+print(f"druga kateta je: {p_trikotnik(kateta1=k1, hipotenuza=h)}")
+
+
+# funkcija
+def pozdrav(ime, starost):
+  print("pozdravljeni {ime}")
+  if int(starost) < 18:
+    print("premlad si")
+  else:
+    print("starček")
+
+uporabnik = input("kako ti je ime? ")
+starost = input("koliko si star? ")
+pozdrav(ime=uporabnik, starost=starost) # tudi če je vrstni red v klicu funkcije drugačen kot pri definiciji same funkcije, ne bo Error
+
+
+"""
+Program združuje vse, kar podpira Python v funkcijah
+poleg osnov (posredovanje parametrov, vračanje rezultata)
+lahko parametre tudi poimenujemo, pri čemer nam ni treba
+posredovati vseh (privzete vrednosti)
+
+Kaj je ARGUMENT in kaj PARAMETER?
+Parameter je posrdovana oz. sprejeta stvar V FUNKCIJI
+argument je to, kar posredujem noter
+
+(ista stvar z drugega zornega kota)
+
+"""
+
+# asistentova verzija programa
+def ptrikotnik(kateta1=0, kateta2=0, hipotenuza=0):
+    # tukaj operiram s PARAMETRI (lokalne spremenljivke, ki
+    # izhajajo iz sprejetih podatkov) -kateta1, kateta2,
+    # hipotenuza
+
+    # POSREDUJEM SI PO DVE:
+    # K1, K2 za izračnu hipotenuze
+    # Kx, hipotenuza za izračun katete 2
+
+    # če si posredujem hipotenuzo in kateto 2, kateto 2
+    # preslikam v kateto 1
+    if kateta2 != 0 and kateta1 == 0 and hipotenuza != 0:
+        kateta1 = kateta2
+        kateta2 = 0
+
+    if kateta2 == 0 and kateta1 != 0 and hipotenuza != 0:
+        return ((hipotenuza**2)-(kateta1**2))**0.5
+
+    elif kateta1 != 0 and kateta2 != 0 and hipotenuza == 0:
+        return ((kateta1**2)+(kateta2**2))**0.5
+
+    if ((kateta1 == 0 and kateta2 == 0) or
+        (kateta1 == 0 and hipotenuza == 0) or
+            (kateta2 == 0 and hipotenuza == 0)):
+        return "NE MOREM IZRAČUNATI SAMO Z ENIM PARAMETROM"
+
+
+k1 = int(input("Vnesi kateto 1: "))
+k2 = int(input("Vnesi kateto 2: "))
+
+print("Hipozenuza je: ", ptrikotnik(kateta1=k1, kateta2=k2))
+
+
+h = int(input("Vnesi hipotenuzo: "))
+k = int(input("Vnesi kateto: "))
+
+print("Druga kateta je: ", ptrikotnik(kateta1=k, hipotenuza=h))
